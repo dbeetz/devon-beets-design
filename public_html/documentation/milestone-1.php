ea9fba737a83d15b9760bc9dedc093a7e2763a72
Devon Beets <dbeetzz@gmail.com>
2016-07-18 09:32:46 -0600

    Updated Needs section

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,7 +18,7 @@
 		<h4>Attitudes and Behaviors</h4>
 		<p>A busy man using the Internet to advertise for an entry to mid level software developer that he needs to help with offloading some extra work he has for his team, building software on a new project while he must maintain and supervise.</p>
 		<h4>Frustrations and Needs</h4>
-		<p>Must fill a position to perform entry to mid level web coding and front-end maintenance for their clients’ websites to support and supplement senior web developer’s projects.</p>
+		<p>Must fill a position to perform entry to mid level web coding and front-end maintenance for their clients’ websites to support and supplement senior web developer’s projects. Has received many applications for his opening, which he must sort through carefully to make sure that he makes the right choice while keeping his time efficient.</p>
 		<h4>Goals</h4>
 		<p>To hire a qualified individual as soon as possible, to keep up with the workload. Will spend the amount of time necessary to compile a list of eligible applicants based on their resume, online professional presence such as LinkedIn, and personal projects before setting to make contact by email and phone.</p>
 	</body>
