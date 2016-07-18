be7d5967e4517e18747df7157afe7b90d5ff5a23
Devon Beets <dbeetzz@gmail.com>
2016-07-18 09:44:08 -0600

    Added Purpose, Audience, and Goal section

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,6 +6,13 @@
 	</head>
 	<body>
 		<h1><strong>Milestone: 1</strong></h1>
+		<h2><strong>Purpose, Audience & Goal</strong></h2>
+		<h4>Purpose</h4>
+		<p>The purpose of my website is to showcase my design skills with a focus on front-end web development</p>
+		<h4>Audience</h4>
+		<p>This site is intended to be viewed by IT professionals, hiring managers, and other web developers as well as businesses looking for a freelancer.</p>
+		<h4>Goal</h4>
+		<p>The main focus of the site is to sharpen my web development front end skills, create professional contacts in the web development community, and get in touch with job opportunities.</p>
 		<h2><strong></strong>Persona</strong></h2>
 		<h4>Name</h4>
 		<p>Mark Lagann</p>
@@ -21,5 +28,9 @@
 		<p>Must fill a position to perform entry to mid level web coding and front-end maintenance for their clients’ websites to support and supplement senior web developer’s projects. Has received many applications for his opening, which he must sort through carefully to make sure that he makes the right choice while keeping his time efficient.</p>
 		<h4>Goals</h4>
 		<p>To hire a qualified individual as soon as possible, to keep up with the workload. Will spend the amount of time necessary to compile a list of eligible applicants based on their resume, online professional presence such as LinkedIn, and personal projects before setting to make contact by email and phone.</p>
+		<h2><strong>Use Case</strong></h2>
+		<p>I have sent a response to Mark's post online, with my resume and cover letter attached as well as a link to my personal website used for self branding and to showcase my portfolio. After a couple of minutes analyzing my source code and front end focus, decides to check upon my social media and LinkedIn presence.<br /></p>
+		<p>Upon viewing my LinkedIn, Facebook, and Twitter profiles, Mark decides to pencil in my name, email and phone number for future contact to perform a short phone interview.<br /></p>
+		<p> <br /></p>
 	</body>
 </html>
