4104ff47f7ce6114d2e7e25e4883467eca67d841
Devon Beets <dbeetzz@gmail.com>
2016-07-18 09:30:15 -0600

    Added Persona headings and text

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,21 @@
 		<title>Devon Beets Design</title>
 	</head>
 	<body>
-		<h1>Milestone: 1</h1>
+		<h1><strong>Milestone: 1</strong></h1>
+		<h2><strong></strong>Persona</strong></h2>
+		<h4>Name</h4>
+		<p>Mark Lagann</p>
+		<h4>Age</h4>
+		<p>36</p>
+		<h4>Occupation</h4>
+		<p>Web developer and IT team lead for a medium sized firm that builds and maintains websites for various contracts in a business to business model.</p>
+		<h4>Technology</h4>
+		<p>Dell XPS 13 laptop PC for work at home and elsewhere, Lenovo desktop PC at office for at work productivity, and Samsung Galaxy S6 Edge.</p>
+		<h4>Attitudes and Behaviors</h4>
+		<p>A busy man using the Internet to advertise for an entry to mid level software developer that he needs to help with offloading some extra work he has for his team, building software on a new project while he must maintain and supervise.</p>
+		<h4>Frustrations and Needs</h4>
+		<p>Must fill a position to perform entry to mid level web coding and front-end maintenance for their clients’ websites to support and supplement senior web developer’s projects.</p>
+		<h4>Goals</h4>
+		<p>To hire a qualified individual as soon as possible, to keep up with the workload. Will spend the amount of time necessary to compile a list of eligible applicants based on their resume, online professional presence such as LinkedIn, and personal projects before setting to make contact by email and phone.</p>
 	</body>
 </html>
