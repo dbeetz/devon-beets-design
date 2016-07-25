f22b352ffc6461c43b7e0831c531360f8bf979ca
Devon Beets <dbeetzz@gmail.com>
2016-07-24 20:35:14 -0600

    Adding milestone 2 documentation and php file

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<title>Devon Beets Design Milestone Two</title>
+	</head>
+</html>
+<body>
+	<h1><strong>Milestone: 2</strong></h1>
+	<h2>Sitemap</h2>
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</body>
