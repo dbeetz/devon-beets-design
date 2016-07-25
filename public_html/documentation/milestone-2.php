0daf5238ce3d4d1648614bf9ae67bdc138c92893
Devon Beets <dbeetzz@gmail.com>
2016-07-24 20:47:14 -0600

    Filled out some sitemapping documentation for milestone 2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,14 +8,24 @@
 <body>
 	<h1><strong>Milestone: 2</strong></h1>
 	<h2>Sitemap</h2>
+	<h3>Home Page</h3>
 	<ul>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
+		<li>"DB Design" graphic</li>
+		<li>Photograph</li>
+		<li>About Me</li>
+		<li>Great Design!</li>
+	</ul>
+	<h3>Resume</h3>
+	<ul>
+		<li>PDF Document with resume</li>
+		<li>Must write and add technical resume.</li>
+		<li>Eventually add portfolio to this section with graphical work when the skills are acquired.</li>
+		<li>Add projects such as Capstone.</li>
+	</ul>
+	<h3>Contact</h3>
+	<ul>
+		<li>Name, number and email</li>
+		<li>Facebook, LinkedIn and Twitter links</li>
+		<li>Fabricate my own email for my website and contact form (@dbeets.com or something :D) </li>
 	</ul>
 </body>
