f176cd95d7f93a000fc12c6609d5fb40b18d5e28
Devon Beets <dbeetzz@gmail.com>
2016-08-03 12:14:03 -0600

    Edited content to be one page layout, a favorable design strategy as opposed to multi page layout

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,31 +8,32 @@
 <body>
 	<h1><strong>Milestone: 2</strong></h1>
 	<h2>Sitemap</h2>
-	<h3>Home Page</h3>
+	<h3>One Page Layout with three parts:</h3>
+	<h4>Introduction</h4>
 	<ul>
 		<li>"DB Design" graphic</li>
-		<li>Photograph</li>
+		<li>Photograph in Jumbotron</li>
 		<li>About Me</li>
 		<li>Great Design with CSS and JavaScript!</li>
 	</ul>
-	<h3>Resume and Portfolio</h3>
+	<h4>Resume and Portfolio</h4>
 	<ul>
 		<li>PDF Document with resume</li>
-		<li>Must write and add technical resume.</li>
+		<li>Must write and add technical resume with web-friendly format.</li>
 		<li>Eventually add portfolio to this section with graphical work when the skills are acquired.</li>
 		<li>Add projects such as Capstone.</li>
 	</ul>
-	<h3>Contact</h3>
+	<h4>Contact</h4>
 	<ul>
 		<li>Name, number and email</li>
 		<li>Facebook, LinkedIn and Twitter links</li>
-		<li>Fabricate my own email for my website and contact form (@dbeets.com or something :D) </li>
+		<li>Fabricate my own email for my website and contact form, devon@devonbeetsdesign.com! :) </li>
 	</ul>
 	<h2><strong>Content Strategy</strong></h2>
-	<h3>Home Page</h3>
+	<h4>Introduction</h4>
 	<p>A stylish introduction to myself as a front end web developer with a picture, my personal graphic and a short description about myself!</p>
-	<h3>Resume and Portfolio</h3>
+	<h4>Resume and Portfolio</h4>
 	<p>A couple of items, first a PDF document with my resume and previous work history along with a technical resume and skillset. Then, also eventually once I have Photoshop and Illustrator skills I would like to add an interactive gallery of graphics and photographs.</p>
-	<h3>Contact</h3>
+	<h4>Contact</h4>
 	<p>A list of my contact information including my phone number, emails, Twitter, Facebook, LinkedIn, and any other social media or other forms of contact along with a form they can use to contact me along with a captcha!</p>
 </body>
