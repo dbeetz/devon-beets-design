17c42734cd0c3dc334c3b2dd27504b77ab45ce84
Devon Beets <dbeetzz@gmail.com>
2016-08-21 21:39:51 -0600

    set up some basic structure for the main page of DBDesign.com!

---
+++ public_html/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Denk+One|Sansita+One" rel="stylesheet">
+
+		<!-- Custom CSS  -->
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+		<link rel="stylesheet" href="css/sticky-footer.css" type="text/css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery - required for Bootstrap Components -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Devon Beets Design</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<!-- Begin Navbar -->
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">
+						<img src="images/dbfavicon.ico" alt="DBFavicon">
+					</a>
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="http://how-i-met-your-mother.wikia.com/wiki/Challenge_Accepted_(catchphrase)">Challenge Accepted!</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="http://www.google.com">About Me</a></li>
+						<li><a href="http://www.pokemon.com/us/">Resume</a></li>
+						<li><a href="https://www.reddit.com/">Contact</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+		</div>
+		</nav>
+			</header>
+		<!-- Begin Jumbotron -->
+		<section id="introduction">
+			<div class="container">
+				<div class="jumbotron text-center">
+					<h1>Devon Beets</h1>
+					<p>Web Designer</p>
+				</div>
+			</div>
+		</section>
+
+
+		</div>
+	</body>
